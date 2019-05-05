e2cafa91c7416502b08e7f5da613c6f1711267d2
zmx <zmx0142857@qq.com>
2019-05-05 15:22:48 +0800

    edit game.h sokoban.c add morse.cpp

--- game.h
+++ game.h
@@ -1,13 +1,91 @@
+/* header file for c games played on terminal */
 #ifndef Game_h
 #define Game_h
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>		// sleep()
+#include <stdbool.h>	// bool
+#include <signal.h>		// signal()
+
+/* windows */
+
+#ifdef __MINGW32__
+
+#include <conio.h>
+#include <windows.h>
+
+HANDLE cmd;
+#define game_init()\
+	do {\
+		cmd = GetStdHandle(STD_OUTPUT_HANDLE);\
+		signal(SIGINT, SIG_IGN);\
+	} while (0)
+
+#define screen_clear()		system("cls")
+//#define sleep(ms)			Sleep(ms)
+
+#define set_color(fg)		SetConsoleTextAttribute(cmd, fg)
+#define set_colors(fg, bg)	SetConsoleTextAttribute(cmd, (bg<<4)|fg)
+
+#define BG_BLACK	0x0
+#define BG_BLUE		0x1
+#define BG_GREEN	0x2
+#define BG_CYAN		0x3
+#define BG_RED		0x4
+#define BG_MAGENTA	0x5
+#define BG_YELLOW	0x6
+#define BG_WHITE	0x7
+#define FG_BLACK	0x8
+#define FG_BLUE		0x9
+#define FG_GREEN	0xa
+#define FG_CYAN		0xb
+#define FG_RED		0xc
+#define FG_MAGENTA	0xd
+#define FG_YELLOW	0xe
+#define FG_WHITE	0xf
+
+/*
+void screen_size(int y, int x)
+{
+    COORD size = {x, y};
+    SetConsoleScreenBufferSize(cmd, size); 	// set buffer size
+    SMALL_RECT rect = {0,0, x-1, y-1};
+    SetConsoleWindowInfo(cmd, 1, &rect);   	// set window size
+}
+*/
+
+void screen_size(unsigned height, unsigned width)
+{
+	char buf[100];
+	sprintf(buf, "mode con lines=%d cols=%d", height, width);
+	system(buf);
+	screen_clear();
+}
+
+#define cursor_goto(y,x)\
+	do {\
+		COORD pos = {x, y};\
+		SetConsoleCursorPosition(cmd, pos);\
+	} while (0)
+
+#endif // __MINGW32__
+
+// ---- linux -------------------------------------------------
+
+#ifdef __linux__
+
 #include <sys/ioctl.h>	// winsize, ioctl()
 #include <termios.h>	// termios
 
-struct winsize tty;
-#define get_ttysize()	ioctl(0, TIOCGWINSZ, &tty);
+#define game_init()			signal(SIGINT, SIG_IGN)
 
+struct winsize tty;
+#define get_ttysize()		ioctl(0, TIOCGWINSZ, &tty)
+#define screen_size(y, x)
+#define Sleep(ms)			usleep(ms*1000)
+
+// enable/disable buffered I/O
 void toggle_flush()
 {
 	static struct termios on, off;
@@ -28,6 +106,16 @@ void toggle_flush()
 	flush_on = !flush_on;
 }
 
+int getch()
+{
+	toggle_flush();
+	int ret = getchar();
+	toggle_flush();
+	return ret;
+}
+
+// POSIX escape sequences
+
 #define STYLE_RESET			"\033[0m"
 #define STYLE_BRIGHT		"\033[1m"
 #define STYLE_DIM			"\033[2m"
@@ -36,25 +124,89 @@ void toggle_flush()
 #define STYLE_REVERSE		"\033[7m"
 #define STYLE_HIDDEN		"\033[8m"
 
-#define BLACK				"0"
-#define RED					"1"
-#define GREEN				"2"
-#define YELLOW				"3"
-#define BLUE				"4"
-#define MAGENTA				"5"
-#define CYAN				"6"
-#define WHITE				"7"
-#define COLOR(color)		"\033[3" color "m"
-#define COLORS(fg, bg)		"\033[3" fg ";4" bg "m"
+#define FG_BLACK			"30"
+#define FG_RED				"31"
+#define FG_GREEN			"32"
+#define FG_YELLOW			"33"
+#define FG_BLUE				"34"
+#define FG_MAGENTA			"35"
+#define FG_CYAN				"36"
+#define FG_WHITE			"37"
+#define BG_BLACK			"40"
+#define BG_RED				"41"
+#define BG_GREEN			"42"
+#define BG_YELLOW			"43"
+#define BG_BLUE				"44"
+#define BG_MAGENTA			"45"
+#define BG_CYAN				"46"
+#define BG_WHITE			"47"
 
-#define screen_clear()		printf("\033[2J\033[0;0H")
+#define COLOR(fg)			"\033[" fg "m"
+#define COLORS(fg, bg)		"\033[" fg ";" bg "m"
+#define set_color(fg)		printf("\033[%sm", fg)
+#define set_colors(fg, bg)	printf("\033[%s;%sm", fg, bg)
+
+#define screen_clear()		printf("\033[2J\033[1;1H")
 #define line_clear()		printf("\033[K")
 
-#define cursor_goto(y,x)	printf("\033[%d;%dH", y, x)
+#define cursor_goto(y,x)	printf("\033[%d;%dH", y+1, x+1)
 #define cursor_cr()			pritnf("\r")
 #define cursor_up(n)		printf("\033[%dA", n)
 #define cursor_down(n)		printf("\033[%dB", n)
 #define cursor_right(n)		printf("\033[%dC", n)
 #define cursor_left(n)		printf("\033[%dD", n)
+
+#endif // __linux__
+
+#define mvprint(y, x, args...)\
+	do {\
+		cursor_goto(y, x);\
+		printf(args);\
+		cursor_goto(0, 0);\
+	} while (0)
+
+#define color_print(fg, args...)\
+	do {\
+		set_color(fg);\
+		printf(args);\
+	} while (0)
+
+#define colors_print(fg, bg, args...)\
+	do {\
+		set_colors(fg, bg);\
+		printf(args);\
+	} while (0)
+
+#define color_mvprint(fg, y, x, args...)\
+	do{\
+		set_color(fg);\
+		mvprint(y, x, args);\
+	} while (0)
+
+#define colors_mvprint(fg, bg, y, x, args...)\
+	do{\
+		set_colors(fg, bg);\
+		mvprint(y, x, args);\
+	} while (0)
+
+#define keep_between(value, inf, sup)\
+		  (value < inf ? (value = inf)\
+		: (value > sup ? (value = sup)\
+		: value))
+
+void play_loading(unsigned loop)
+{
+    unsigned i, j;
+    for (i = 0; i != loop; ++i) {
+        printf("LOADING");
+        Sleep(255);
+        for (j = 0; j != 3; ++j) {
+            printf(".");
+            Sleep(255);
+        }
+        printf("\r          \r");
+    }
+	screen_clear();
+}
 
 #endif // Game_h
---
+++ sokoban/sokoban.c
@@ -0,0 +1,316 @@
+#include <string.h> // memset(), strcpy()
+#include <ctype.h> // isdigit()
+#include "../game.h"
+
+// 常量
+#define MAP_W       17
+#define MAP_H       13
+
+#define SPACE       ' '
+#define WALL        '#'
+#define HERO        '@'
+#define BOX         '$'
+#define BOX_ON_STAR '*'
+#define STAR        '.'
+
+#ifdef __MINGW32__
+#define WALL_SHOWN "█"
+#define STAR_SHOWN "★"
+#define ARROWS_SHOWN "↑←↓→"
+#endif
+#ifdef __linux__
+#define WALL_SHOWN "██"
+#define STAR_SHOWN "★ "
+#define ARROWS_SHOWN "↑ ← ↓ → "
+#endif
+
+// 全局变量
+char map[MAP_H][MAP_W] = {{0}};			// 地图
+char map_copy[MAP_H][MAP_W] = {{0}};	// 地图的副本，用于还原被抹去的星形记号
+int hero[2] = {0};						// 人的行列坐标分别是 hero[0], hero[1]
+#define hero_y hero[0]
+#define hero_x hero[1]
+
+char level_name[256];					// 关卡名
+char file_name[256];					// 关卡文件
+int move_cnt = 0;						// 移动的次数
+int push_cnt = 0;						// 推箱子的次数
+bool game_won = 0;						// 关卡是否结束?
+
+// 函数
+
+void map_print()
+{
+    for (int i = 0; i < MAP_H; ++i, printf("\n"))
+        for (int j = 0; j < MAP_W; ++j)
+            switch (map[i][j]) {
+                case HERO: color_print(FG_CYAN, "囧"); break;
+                case WALL: color_print(FG_BLACK, WALL_SHOWN);  break;
+                case BOX:  color_print(FG_BLACK, "田"); break;
+                case BOX_ON_STAR: color_print(FG_YELLOW, "田"); break;
+				case STAR: color_print(FG_YELLOW, STAR_SHOWN);  break;
+                default: printf("  ");
+            }
+	color_print(FG_WHITE,
+		"\n\n"
+		"    关卡 %s    移动%3d   推箱%3d\n\n"
+		"                     R - 重新来过\n"
+		"                 <ESC> - 回主画面\n",
+		level_name, move_cnt, push_cnt
+	);
+	cursor_goto(0, 0);
+}
+
+void scene_welcome()
+{
+	screen_clear();
+
+	set_color(FG_BLUE);
+	char *buf[] = {
+		"   #  #  #    #     #      #####",
+		" #######  ### ###         #",
+		"   #  #  #   # #  # #         #",
+		"   ######  ### ### #######",
+		" ##  #  #      #   #  #       #",
+		" ##  ####  ### ###       #",
+		"   #  ####    #   ###     ##"
+	};
+	for (int i = 0; i < sizeof(buf)/sizeof(char*); ++i) {
+		for (char *p = buf[i]; *p != '\0'; ++p) {
+			if (*p == WALL)
+				printf("田");
+			else
+				putchar(*p);
+		}
+		putchar('\n');
+	}
+	color_print(FG_WHITE, "\n\n\n"
+		"                1  开始游戏\n\n"
+		"                2  选择关卡\n\n"
+		"                3  游戏帮助\n\n"
+		"                4  退出游戏\n");
+}
+
+void scene_help()
+{
+	screen_clear();
+
+	printf("\n\n");
+	color_print(FG_CYAN,  "    囧    ");
+	color_print(FG_WHITE, "    你\n\n");
+
+	color_print(FG_BLACK, "    " WALL_SHOWN "    ");
+	color_print(FG_WHITE, "    墙\n\n");
+
+	color_print(FG_BLACK, "    田    ");
+	color_print(FG_WHITE, "    箱子 你无法同时推动两个箱子！\n\n");
+
+	color_print(FG_YELLOW,"    " STAR_SHOWN "    ");
+	color_print(FG_WHITE, "    目标 将所有箱子推到目标上！\n\n");
+
+	color_print(FG_YELLOW,"    田    ");
+	color_print(FG_WHITE, "    就位的箱子\n\n");
+
+	printf("    " ARROWS_SHOWN "  移动\n\n"
+	       "    R         重新来过\n\n"
+	       "    <ESC>     回主画面\n");
+
+	cursor_goto(0, 0);
+	getch();
+}
+
+bool level_load()
+{
+	FILE *fin = fopen(file_name, "r");
+	if (!fin)
+		return 0;
+	memset(map, SPACE, sizeof(map));
+	memset(map_copy, SPACE, sizeof(map_copy));
+	int ch, x = 0, y = 0;
+	while ((ch = fgetc(fin)) != EOF) {
+		switch (ch) {
+			case '\n':
+				x = 0;
+				++y;
+				break;
+			case HERO:
+				hero_y = y;
+				hero_x = x;
+				map[y][x++] = ch;
+				break;
+			case STAR: case BOX_ON_STAR:
+				map_copy[y][x] = STAR;
+				map[y][x++] = ch;
+				break;
+			case WALL: case BOX:
+				map[y][x++] = ch;
+				break;
+			default:
+				map[y][x++] = SPACE;
+		}
+	}
+	fclose(fin);
+
+    move_cnt = 0;
+    push_cnt = 0;
+    game_won = 0;
+
+	Sleep(255);
+	screen_clear();
+    map_print();
+	return 1;
+}
+
+bool is_game_won()
+{
+	int i, j;
+	for (i = 0; i < MAP_H; ++i)
+		for (j = 0; j < MAP_W; ++j)
+			if (map[i][j] == BOX)
+				return 0;
+    return 1;
+}
+
+// 若要回到主画面, 则返回 1
+bool control()
+{
+    char key = getch();
+    if (key == 'u' || key == 'U') {			//  U - 撤消
+		;
+    } else if (key == 'r' || key == 'R') {	//  R - 重新来过
+        level_load();
+#ifdef __MINGW32__
+    } else if (key == 27) {					//  <ESC> - 回主画面
+        return 1;
+    } else if (key == -32) {				// 方向键
+        key = getch();
+        if (key != 'H' && key != 'P' && key != 'K' && key != 'M')
+            return 0;
+
+        int dy = key == 'H' ? -1 : (key == 'P' ? 1 : 0);
+        int dx = key == 'K' ? -1 : (key == 'M' ? 1 : 0);
+#endif
+#ifdef __linux__
+    } else if (key == 27) {					// 方向键
+        key = getch();
+		if (key != '[')
+			return 1;
+        key = getch();
+        if (key != 'A' && key != 'B' && key != 'C' && key != 'D')
+            return 0;
+
+        int dy = key == 'A' ? -1 : (key == 'B' ? 1 : 0);
+        int dx = key == 'D' ? -1 : (key == 'C' ? 1 : 0);
+#endif
+        int next_y = hero_y + dy;
+        int next_x = hero_x + dx;
+
+        bool move = map[next_y][next_x] == SPACE
+                 || map[next_y][next_x] == STAR;
+        bool push = map[next_y][next_x] == BOX
+                 ||	map[next_y][next_x] == BOX_ON_STAR;
+        bool has_space = map[next_y + dy][next_x + dx] == SPACE;
+        bool has_star = map[next_y + dy][next_x + dx] == STAR;
+        bool update_hero = 0;
+
+        if (move) {
+            update_hero = 1;
+        } else if (push && (has_space || has_star)) {
+            update_hero = 1;
+            map[next_y + dy][next_x + dx] = has_star ? BOX_ON_STAR : BOX;
+            color_mvprint(
+				(has_star ? FG_YELLOW : FG_BLACK),
+				next_y + dy, 2*(next_x + dx),
+				"田"
+			);
+            ++push_cnt;
+        }
+
+        if (update_hero) {
+            // map_copy的真正用意：恢复被抹去的星记号
+            bool star = map_copy[hero_y][hero_x] == STAR;
+            map[hero_y][hero_x] = star ? STAR : SPACE;
+            map[next_y][next_x] = HERO;
+            color_mvprint(
+				FG_YELLOW,
+				hero_y, 2*hero_x,
+				star ? "★" : "  "
+			);
+            color_mvprint(
+				FG_CYAN,
+				next_y, 2*next_x,
+				"囧"
+			);
+            hero_y += dy;
+            hero_x += dx;
+            ++move_cnt;
+
+			game_won = is_game_won();
+			set_color(FG_WHITE);
+			mvprint(MAP_H + 2, 20, "%2d", move_cnt);
+			mvprint(MAP_H + 2, 30, "%2d", push_cnt);
+        }
+    }
+    return 0;
+}
+
+// max level number is 999
+void play(const char *levelname)
+{
+	strcpy(level_name, levelname);
+	// check if levelname is number
+	bool is_number = 1;
+	for (const char *p = levelname; *p != '\0'; ++p) {
+		if (!isdigit(*p)) {
+			is_number = 0;
+			break;
+		}
+	}
+	if (is_number) {
+		int i = atoi(levelname);
+		sprintf(file_name, "level/%03d.level", i);
+		while (level_load()) {
+			do {
+				if (control())
+					return;
+			} while (!game_won);
+			sprintf(file_name, "level/%03d.level", ++i);
+			sprintf(level_name, "%d", i);
+		}
+	} else {
+		sprintf(file_name, "level/%s.level", levelname);
+		if (level_load()) {
+			do {
+				if (control())
+					return;
+			} while (!game_won);
+		}
+	}
+}
+
+void scene_level_select()
+{
+	screen_clear();
+	printf("\n\n\n\n\n\n\n\n\t\t选择关卡: ");
+	char buf[256];
+	scanf("%s", buf);
+	play(buf);
+}
+
+int main()
+{
+	char ch = ' ';
+	game_init();
+	screen_size(20, 48);
+    while (1) {
+        scene_welcome();
+		ch = getch();
+        switch (ch) {
+            case '1': play("1"); break;
+            case '2': scene_level_select();  break;
+            case '3': scene_help();  break;
+            case '4': return 0;
+        }
+    }
+    return 0;
+}
