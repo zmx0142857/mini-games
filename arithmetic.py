f6d596ab3f83e97de61288671aac9c78b323927c
zmx <892298182@qq.com>
2020-07-03 18:04:07 +0800

    20200703 backup edit arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -9,14 +9,25 @@ correct = 0
 mistake = 0
 mistakes = []
 begin = time()
+beginq = begin
 a = 12
 b = 99
+speed = {}
+
+def stat():
+    total = correct + mistake
+    print("\rcorrect: %d/%d" % (correct, total))
+    if total > 0:
+        print("speed: %f questions per minute" % (60*correct/(time()-begin)))
+        print('\n'.join('%d: %f' % e for e in sorted([(k, 60*speed[k][1]/speed[k][0]) for k in speed], key=(lambda t:t[1]), reverse=True)))
+        if mistakes:
+            print("mistakes: %s" % ', '.join('%d*%d' % elem for elem in set(mistakes)))
+
 if len(argv) > 2:
     a, b = int(argv[1]), int(argv[2])
-while True:
+for cnt in range(30):
     if len(mistakes) > 0 and randint(0,6) == 0:
-        elem = choice(mistakes)
-        i, j = elem['i'], elem['j']
+        i, j = choice(mistakes)
     else:
         i, j = randint(a,b), randint(2,9)
     ans = i * j
@@ -24,18 +35,22 @@ while True:
         try:
             user = int(input("%d x %d = " % (i, j)))
         except EOFError:
-            total = correct + mistake
-            print("\rcorrect: %d/%d" % (correct, total))
-            if total > 0:
-                print("speed: %f questions per minute" % (60*correct/(time()-begin)))
+            stat()
             exit()
         except ValueError:
             print("invalid input")
             continue
         if user == ans:
             correct += 1
+            now = time()
+            if i in speed:
+                speed[i] = (speed[i][0]+now-beginq, speed[i][1]+1)
+            else:
+                speed[i] = (now-beginq, 1)
+            beginq = now
         else:
             mistake += 1
             print("answer: %d" % ans)
-            mistakes.append({'i':i, 'j':j})
+            mistakes.append((i,j))
         break
+stat()
