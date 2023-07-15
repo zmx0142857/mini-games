45e439720091fcaf88b18824e9f14da541bdf42e
zmx <892298182@qq.com>
2023-07-15 17:48:11 +0800

    edit typing.cpp: record recent 20 speed

--- c/typing/typing.cpp
+++ c/typing/typing.cpp
@@ -1,15 +1,11 @@
 /* bug:
  * 1. 窗口过小，容不下一段文字时文字不显示
  * 2. 窗口由大变小时光标位置不对
- *
- * TODO:
- * 1. 读取配置文件，加载打字进度
- * 2. 保存历史打字速度
  */
 #include "game.h"
-#include "str.h"	// String
-#include <clocale>	// setlocale()
-#include <cctype>	// isspace()
+#include "str.h"    // String
+#include <clocale>  // setlocale()
+#include <cctype>   // isspace()
 #include <cstring>  // strcmp()
 #include <ctime>    // rand()
 #include <vector>
@@ -23,43 +19,43 @@ using namespace std;
 
 class Article {
 
-	vector<String> text;
-	vector<int> widths;
+    vector<String> text;
+    vector<int> widths;
 
 public:
     void read_stream(const string &filename, ifstream &ifs) {
-		ifs.open(filename, ios::in);
-		if (!ifs) {
-			cerr << "error: cannot open file '" << filename << "'. "
-				"does this file exist?\n";
-			exit(1);
-		}
+        ifs.open(filename, ios::in);
+        if (!ifs) {
+            cerr << "error: cannot open file '" << filename << "'. "
+                "does this file exist?\n";
+            exit(1);
+        }
     }
 
-	// 载入
-	void read(const string &filename, int begin = 1, int end = 0x7fffffff) {
+    // 载入
+    void read(const string &filename, int begin = 1, int end = 0x7fffffff) {
         ifstream ifs;
         read_stream(filename, ifs);
-		String line;
+        String line;
         text.clear();
-		for (int i = 1; getline(ifs, line); ++i) {
+        for (int i = 1; getline(ifs, line); ++i) {
             if (i < begin) continue;
             else if (i > end) break;
-			text.push_back(line);
-			// 计算一行文字的宽度并缓存
-			int w = 0;
-			for (size_t i = 0; i < line.size(); ++i) {
-				if (line[i].is_ascii())
-					w += 1;
-				else
-					w += 2;
-			}
-			if (line.size() == 0)
-				w = 1; // 空行也有一格的高度
-			widths.push_back(w);
-		}
+            text.push_back(line);
+            // 计算一行文字的宽度并缓存
+            int w = 0;
+            for (size_t i = 0; i < line.size(); ++i) {
+                if (line[i].is_ascii())
+                    w += 1;
+                else
+                    w += 2;
+            }
+            if (line.size() == 0)
+                w = 1; // 空行也有一格的高度
+            widths.push_back(w);
+        }
         ifs.close();
-	}
+    }
 
     // 随机文本
     void rand(const string &filename, int rand_cnt = 10) {
@@ -87,24 +83,24 @@ public:
         ifs.close();
     }
 
-	int size() const {
-		return text.size();
-	}
+    int size() const {
+        return text.size();
+    }
 
-	const String &get(int line) const {
-		return text[line];
-	}
+    const String &get(int line) const {
+        return text[line];
+    }
 
-	// 第 line 行第 col 字
-	Char get(size_t line, size_t col) const {
-		if (line < text.size() && col < text[line].size())
-			return text[line][col];
-		return Char();
-	}
+    // 第 line 行第 col 字
+    Char get(size_t line, size_t col) const {
+        if (line < text.size() && col < text[line].size())
+            return text[line][col];
+        return Char();
+    }
 
-	int width(int line) {
-		return widths[line];
-	}
+    int width(int line) {
+        return widths[line];
+    }
 
     bool is_end(int line, int col) const {
         return line == size() - 1 && col == get(line).size();
@@ -113,13 +109,13 @@ public:
 };
 
 enum Status {
-	C1, // 英文正确
-	C2, // 中文正确
-	E1, // 英文错误
-	E2, // 中文错误
-	E3, // 误在英文处输入中文
-	E4, // 误在中文处输入英文
-	CR  // 回车
+    C1, // 英文正确
+    C2, // 中文正确
+    E1, // 英文错误
+    E2, // 中文错误
+    E3, // 误在英文处输入中文
+    E4, // 误在中文处输入英文
+    CR  // 回车
 };
 enum Ctrl {NONE, CONTINUE, BREAK, RETURN};
 
@@ -127,14 +123,14 @@ class Game {
     const char *filename;
     int begin, end, rand_cnt;
     bool need_restart;
-	int line, col, x;
-	int loaded_cnt;
-	struct { int correct = 0, error = 0, backspace = 0; } statistics;
-	stack<Status> undo;
-	Article article;
+    int line, col, x;
+    int loaded_cnt;
+    struct { int correct = 0, error = 0, backspace = 0; } statistics;
+    stack<Status> undo;
+    Article article;
 
 public:
-	Game(const char *filename, int begin = 1, int end = 0x7fffffff, int rand_cnt = -1):
+    Game(const char *filename, int begin = 1, int end = 0x7fffffff, int rand_cnt = -1):
         filename(filename), begin(begin), end(end), rand_cnt(rand_cnt)
     {
         do {
@@ -151,7 +147,7 @@ private:
         this->need_restart = false;
         this->statistics = { 0, 0, 0 };
 
-		setlocale(LC_ALL, "");		// 使用系统 locale
+        setlocale(LC_ALL, "");      // 使用系统 locale
         if (rand_cnt == -1) {
             article.read(filename, begin, end);
         } else {
@@ -159,106 +155,120 @@ private:
         }
 
         print();
-		play();
+        play();
     }
 
     // 在画面范围内打印
     void print() {
-		screen_clear();
-		get_ttysize();
-		cout << STYLE_DIM;
-		if (loaded_cnt < article.size()) {
-			int h = height(loaded_cnt);
-			while (loaded_cnt < article.size() && h < tty.ws_row) {
-				cout << article.get(loaded_cnt++) << '\n';
-				h += height(loaded_cnt);
-			}
-			cursor_up(h - height(loaded_cnt));
-		}
-		cout << STYLE_RESET;
+        screen_clear();
+        get_ttysize();
+        cout << STYLE_DIM;
+        if (loaded_cnt < article.size()) {
+            int h = height(loaded_cnt);
+            while (loaded_cnt < article.size() && h < tty.ws_row) {
+                cout << article.get(loaded_cnt++) << '\n';
+                h += height(loaded_cnt);
+            }
+            cursor_up(h - height(loaded_cnt));
+        }
+        cout << STYLE_RESET;
     }
 
-	void play() {
-		toggle_flush();
-		int c = getchar();
-		long begin = time_ms();
-		control(c);
+    void play() {
+        toggle_flush();
+        int c = getchar();
+        long begin = time_ms();
+        control(c);
         if (!need_restart) {
             summary(begin);
         }
-		toggle_flush();
-	}
+        toggle_flush();
+    }
 
-	static int width(const Char &c) {
-		return c.is_ascii() ? 1 : 2;
-	}
+    static int width(const Char &c) {
+        return c.is_ascii() ? 1 : 2;
+    }
 
-	int width(int line) {
-		return article.width(line);
-	}
+    int width(int line) {
+        return article.width(line);
+    }
 
-	int height(int line) {
-		return width(line) / tty.ws_col + 1; // 记得刷新终端尺寸
-	}
+    int height(int line) {
+        return width(line) / tty.ws_col + 1; // 记得刷新终端尺寸
+    }
 
-	void update() {
-		// 用户已打完的行数距离已打印行数小于 4 时更新一行
-		if (loaded_cnt < line + 4 && loaded_cnt < article.size()) {
-			get_ttysize();
-			cout << STYLE_DIM;
-			// 光标下移三行
-			int k = 0;
-			while (k < 3) cursor_down(height(line+(k++)));
-			// 打印新的一行
-			const int h = height(loaded_cnt);
-			cout << article.get(loaded_cnt++) << endl;
-			// 光标归位
-			cursor_up(h);
-			while (k) cursor_up(height(line+(--k)));
-			cout << STYLE_RESET;
-		}
-	}
+    void update() {
+        // 用户已打完的行数距离已打印行数小于 4 时更新一行
+        if (loaded_cnt < line + 4 && loaded_cnt < article.size()) {
+            get_ttysize();
+            cout << STYLE_DIM;
+            // 光标下移三行
+            int k = 0;
+            while (k < 3) cursor_down(height(line+(k++)));
+            // 打印新的一行
+            const int h = height(loaded_cnt);
+            cout << article.get(loaded_cnt++) << endl;
+            // 光标归位
+            cursor_up(h);
+            while (k) cursor_up(height(line+(--k)));
+            cout << STYLE_RESET;
+        }
+    }
 
-	void summary(long begin)
-	{
-		int s = time_ms() - begin;
-		int mm = s / 60000;
-		int ss = s / 1000;
+    void summary(long begin)
+    {
+        int s = time_ms() - begin;
+        int mm = s / 60000;
+        int ss = s / 1000;
         int ms = s % 1000;
-		int typed = statistics.correct + statistics.error;
+        int typed = statistics.correct + statistics.error;
 
-		// 滚动到页尾
-		get_ttysize();
-		int sum = 0;
-		while (line < article.size() && sum < tty.ws_row) {
-			int h = height(line++);
-			cursor_down(h);
-			sum += h;
-		}
+        // 滚动到页尾
+        get_ttysize();
+        int sum = 0;
+        while (line < article.size() && sum < tty.ws_row) {
+            int h = height(line++);
+            cursor_down(h);
+            sum += h;
+        }
 
-        float avg;
-        int count;
+        int count = 0;
+	int len = 0;
+	const int recent_count = 20;
+        float recent_speed[recent_count];
+        float avg = 0.0f;
         fstream fs("typing.log", ios::in);
         if (fs) {
             // 存在则读取, 然后清空文件重新打开
-            fs >> avg >> count;
+            fs >> count;
+            len = count < recent_count ? count : recent_count;
+            for (int i = 0; i < len; ++i) {
+                fs >> recent_speed[i];
+            }
             fs.close();
             fs.open("typing.log", ios::out | ios::trunc);
         } else {
             // 不存在则创建
-            avg = 0.0f;
-            count = 0;
             fs.close();
             fs.open("typing.log", ios::out);
         }
 
         float accuracy = 100.0 * statistics.correct / typed;
         float speed = 600.0 * statistics.correct * accuracy / s;
-        avg = (avg * count + speed) / (++count);
-        fs << avg << '\n' << count << endl;
+        fs << count + 1 << '\n';
+        for (int i = 0; i < len; ++i) {
+            avg += recent_speed[i];
+	    // 最多只保留最近 recent_count 条速度记录, 然后取平均
+            if (i > 0 || len < recent_count) {
+                fs << recent_speed[i] << '\n';
+            }
+        }
+        avg = (avg + speed) / (len + 1);
+        fs << speed << endl;
         fs.close();
+        ++count;
 
-		printf(
+        printf(
             "\r--------------------\n"
             "lesson:    %d\n"
             "time:      %dm %ds %dms\n"
@@ -268,162 +278,162 @@ private:
             "typed:     %d\n"
             "correct:   %d\n"
             "backspace: %d\n",
-			count,
+            count,
             mm, ss, ms,
             speed,
             avg,
             accuracy,
-			typed,
+            typed,
             statistics.correct,
-			statistics.backspace
+            statistics.backspace
         );
-	}
+    }
 
-	void control(int key) {
-		string buf;
-		Ctrl ctrl = NONE;
-		// ctrl-d to exit
-		while (key != 4) {
-			if (key == 127) {
-				ctrl = onBackspace();
+    void control(int key) {
+        string buf;
+        Ctrl ctrl = NONE;
+        // ctrl-d to exit
+        while (key != 4) {
+            if (key == 127) {
+                ctrl = onBackspace();
             } else if (key == 21) { // ctrl-u
                 need_restart = true;
                 return;
-			} else if (key == '\n') {
-				ctrl = onReturn();
-			} else {
-				Char answer = article.get(line, col);
-				if (answer) {
-					ctrl = onInput(key, buf, answer);
+            } else if (key == '\n') {
+                ctrl = onReturn();
+            } else {
+                Char answer = article.get(line, col);
+                if (answer) {
+                    ctrl = onInput(key, buf, answer);
                 } else if (article.is_end(line, col)) {
                     // 跟打结束, 按任意键退出
                     ctrl = BREAK;
                 }
-//				else
-//					cout << "ignore: " << key << endl;
-			}
+//              else
+//                  cout << "ignore: " << key << endl;
+            }
 
-			if (ctrl == CONTINUE) continue;
-			else if (ctrl == BREAK) break;
-			else if (ctrl == RETURN) return;
-			key = getchar();
-		}
-	}
+            if (ctrl == CONTINUE) continue;
+            else if (ctrl == BREAK) break;
+            else if (ctrl == RETURN) return;
+            key = getchar();
+        }
+    }
 
-	Ctrl onBackspace() {
-		get_ttysize();
-		if (!undo.empty()) {
-			++statistics.backspace;
-			enum Status stat = undo.top(); undo.pop();
-			switch (stat) {
-				case C1: remove(true, 1);  break;
-				case C2: remove(true, 2);  break;
-				case E1: remove(false, 1); break;
-				case E2: case E4: remove(false, 2); break;
-				case E3: remove(false, 2, true); break;
-				case CR:
-					col = article.get(--line).size();
-					x = article.width(line);
-					cursor_up(1);
-					cursor_right(x % tty.ws_col);
-					break;
-				default: break;
-			}
-		}
-		return NONE;
-	}
+    Ctrl onBackspace() {
+        get_ttysize();
+        if (!undo.empty()) {
+            ++statistics.backspace;
+            enum Status stat = undo.top(); undo.pop();
+            switch (stat) {
+                case C1: remove(true, 1);  break;
+                case C2: remove(true, 2);  break;
+                case E1: remove(false, 1); break;
+                case E2: case E4: remove(false, 2); break;
+                case E3: remove(false, 2, true); break;
+                case CR:
+                    col = article.get(--line).size();
+                    x = article.width(line);
+                    cursor_up(1);
+                    cursor_right(x % tty.ws_col);
+                    break;
+                default: break;
+            }
+        }
+        return NONE;
+    }
 
-	Ctrl onReturn() {
-		if (!article.get(line, col)) {
-			undo.push(CR);
-			++statistics.correct;
-			++line;
-			col = 0;
-			x = 0;
-			putchar('\n');
-			update();
-			if (line == article.size())
-				return BREAK;
-		}
-		return NONE;
-	}
+    Ctrl onReturn() {
+        if (!article.get(line, col)) {
+            undo.push(CR);
+            ++statistics.correct;
+            ++line;
+            col = 0;
+            x = 0;
+            putchar('\n');
+            update();
+            if (line == article.size())
+                return BREAK;
+        }
+        return NONE;
+    }
 
-	Ctrl onInput(char key, string &buf, const Char &answer) {
-		buf += key;
-		istringstream is(buf);
-		Char c;
-		if (is >> c) {
-			buf = "";
-			if (c == answer) {
-				cout << c;
-				add_correct(width(c));
-			} else {
-				cout << COLOR(FG_RED) << c << STYLE_RESET;
-//				cout << answer.ord;
-				add_error(width(c), width(answer));
-			}
-			x += width(c);
-			++col;
-		}
-		return NONE;
-	}
+    Ctrl onInput(char key, string &buf, const Char &answer) {
+        buf += key;
+        istringstream is(buf);
+        Char c;
+        if (is >> c) {
+            buf = "";
+            if (c == answer) {
+                cout << c;
+                add_correct(width(c));
+            } else {
+                cout << COLOR(FG_RED) << c << STYLE_RESET;
+//              cout << answer.ord;
+                add_error(width(c), width(answer));
+            }
+            x += width(c);
+            ++col;
+        }
+        return NONE;
+    }
 
-	void add_correct(int w) {
-		if (w == 1)
-			undo.push(C1); // 英文正确
-		else
-			undo.push(C2); // 中文正确
-		++statistics.correct;
-	}
+    void add_correct(int w) {
+        if (w == 1)
+            undo.push(C1); // 英文正确
+        else
+            undo.push(C2); // 中文正确
+        ++statistics.correct;
+    }
 
-	void add_error(int w, int w_ans) {
-		if (w == 1) {
-			if (w_ans == 1) {
-				undo.push(E1); // 英文错误
-			} else {
-				undo.push(E4); // 误在中文处输入英文
-				cout << ' '; //! 补齐中文宽度
-			}
-		} else {
-			if (w_ans == 1) {
-				undo.push(E3); // 误在英文处输入中文
-				++col; //! 额外多加1
-			} else {
-				undo.push(E2); // 中文错误
-			}
-		}
-		++statistics.error;
-	}
+    void add_error(int w, int w_ans) {
+        if (w == 1) {
+            if (w_ans == 1) {
+                undo.push(E1); // 英文错误
+            } else {
+                undo.push(E4); // 误在中文处输入英文
+                cout << ' '; //! 补齐中文宽度
+            }
+        } else {
+            if (w_ans == 1) {
+                undo.push(E3); // 误在英文处输入中文
+                ++col; //! 额外多加1
+            } else {
+                undo.push(E2); // 中文错误
+            }
+        }
+        ++statistics.error;
+    }
 
-	void remove(bool correct, int w, bool additional=false) {
-		if (correct)
-			--statistics.correct;
-		else
-			--statistics.error;
-		--col;
-		x -= w;
-		cursor_left(w);
-		cout << STYLE_DIM;
-		if (additional) {
-			cout << article.get(line, col-1) << article.get(line, col);
-			--col; //! 额外多减1
-		} else {
-			cout << article.get(line, col);
-		}
-		cout << STYLE_RESET;
-		cursor_left(w);
-	}
+    void remove(bool correct, int w, bool additional=false) {
+        if (correct)
+            --statistics.correct;
+        else
+            --statistics.error;
+        --col;
+        x -= w;
+        cursor_left(w);
+        cout << STYLE_DIM;
+        if (additional) {
+            cout << article.get(line, col-1) << article.get(line, col);
+            --col; //! 额外多减1
+        } else {
+            cout << article.get(line, col);
+        }
+        cout << STYLE_RESET;
+        cursor_left(w);
+    }
 
 };
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1) {
-		cout << "Typing practice. Choose your favorite article!\n"
-			"usage: typing [filename] [begin line] [end line]\n";
-		return 0;
-	}
-	//logger.open("typing.log", ios::out);
+    if (argc == 1) {
+        cout << "Typing practice. Choose your favorite article!\n"
+            "usage: typing [filename] [begin line] [end line]\n";
+        return 0;
+    }
+    //logger.open("typing.log", ios::out);
 
     srand(time(NULL));
     if (argc == 2) {
@@ -441,6 +451,6 @@ int main(int argc, char *argv[])
             Game game(argv[1], atoi(argv[2]), atoi(argv[3]));
         }
     }
-	return 0;
+    return 0;
 }
 
